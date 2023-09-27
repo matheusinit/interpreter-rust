28de08f723615de39b1b0cd97fe0d47ccca920fe
matheusinit <matheus.oliveira.s@protonmail.com>
2023-09-27 16:19:54 -0300

    feat: ensure json can be read by cli arg using path

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::fs;
+use std::{env, fs};
 
 use crate::eval::eval;
 use crate::file::File;
@@ -8,7 +8,8 @@ pub mod file;
 pub mod structs;
 
 fn main() {
-    let program = fs::read_to_string("./examples/hello.json").unwrap();
+    let input_path = env::args().nth(1).unwrap();
+    let program = fs::read_to_string(input_path).unwrap();
     let program = serde_json::from_str::<File>(&program).unwrap();
 
     let term = program.expression;
